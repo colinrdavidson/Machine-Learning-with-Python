f23bed7ac2a973cd414387da91e0b3ed6b9b72ee
colinrdavidson <colinrdavidson@gmail.com>
2014-01-28 19:25:03 -0500

    More playing

--- pyplot_playing/3dplot.py
+++ pyplot_playing/3dplot.py
@@ -14,31 +14,33 @@ ones = np.matrix(np.ones(X_dat.shape[0])).T
 
 X_dat = np.hstack([ones, X_dat])
 
-X = np.linspace(-5, -3, 100)
-Y = np.linspace(0, 2, 100)
+X = np.linspace(-5, 1, 30)
+Y = np.linspace(-1, 2, 30)
 
 X, Y = np.meshgrid(X,Y)
 
 Z = []
+Z_flat = []
 
 for i in range(0, len(X)):
   Z.append([])
+  Z_flat.append([])
   for j in range(0, len(Y)):
     Z[i].append(compute_cost(X_dat, y_dat, np.matrix( [X[i][j], Y[i][j]] ).T ))
+    Z_flat[i].append(4.48339)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, projection='3d')
-ax.scatter([-3.89530051], [1.19298539], 0, c='r')
+
+ax.plot_wireframe(X, Y, Z)
+ax.plot_surface(X, Y, Z_flat)
+
+ax.scatter(0, 0, compute_cost(X_dat, y_dat, np.matrix( [0, 0] ).T), c='g')
+ax.scatter(-3.89530051, 1.19298539, 4.48339, c='r')
+
 ax.set_xlabel("Theta_0")
 ax.set_ylabel("Theta_1")
 ax.set_zlabel("Cost")
 
-ax.plot_surface(X, Y, Z)
 
 plt.show()
-
-
-
-
-
-
